f490aa65ffb20acb0b2a9083f9eafda492be3c23
gemmechu <gemmechumohammed@gmail.com>
2021-07-28 13:15:35 +0300

    api check

--- app.py
+++ app.py
@@ -1,20 +1,51 @@
-from flask import Flask, jsonify
-from flask_restful import Resource, Api
-from flask_cors import CORS
-import torch
-
+# app.py
+from flask import Flask, request, jsonify
 app = Flask(__name__)
-api = Api(app)
-CORS(app)
 
-class predict (Resource):
-    def get(self):
-        try:
-            l = torch.load('tempmodel.h5')
-            test_values = torch.tensor([[15.0,41.0,85.0,70.0,36.0,30.0,15.0,20.0,16.0,40.0,20.0,15000,15000]])
-            result = torch.argmax(torch.softmax(l(test_values), 1), axis=1)
-            return jsonify({'rating':(result.item())})
-        except:
-            return {'data': 'An Error Occurred during fetching Api'}
+@app.route('/getmsg/', methods=['GET'])
+def respond():
+    # Retrieve the name from url parameter
+    name = request.args.get("name", None)
 
-api.add_resource(predict, '/predict')
+    # For debugging
+    print(f"got name {name}")
+
+    response = {}
+
+    # Check if user sent a name at all
+    if not name:
+        response["ERROR"] = "no name found, please send a name."
+    # Check if the user entered a number not a name
+    elif str(name).isdigit():
+        response["ERROR"] = "name can't be numeric."
+    # Now the user entered a valid name
+    else:
+        response["MESSAGE"] = f"Welcome {name} to our awesome platform!!"
+
+    # Return the response in json format
+    return jsonify(response)
+
+@app.route('/post/', methods=['POST'])
+def post_something():
+    param = request.form.get('name')
+    print(param)
+    # You can add the test cases you made in the previous function, but in our case here you are just testing the POST functionality
+    if param:
+        return jsonify({
+            "Message": f"Welcome {name} to our awesome platform!!",
+            # Add this option to distinct the POST request
+            "METHOD" : "POST"
+        })
+    else:
+        return jsonify({
+            "ERROR": "no name found, please send a name."
+        })
+
+# A welcome message to test our server
+@app.route('/')
+def index():
+    return "<h1>Welcome to our server !!</h1>"
+
+if __name__ == '__main__':
+    # Threaded option to enable multiple instances for multiple user access support
+    app.run(threaded=True, port=5000)
