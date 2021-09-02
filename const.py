73f55ec6301a252275b9401d7b902d75048ab5f5
Gemmechu Mohammed Hassena <gemmechumohammed@gmail.com>
2021-09-02 10:50:50 +0300

    Update const.py

--- const.py
+++ const.py
@@ -1,2 +1,2 @@
-API_KEY = 'AIzaSyDcixkMKgROY2tE_4VLPTioPtDOwbmzfcI'
+API_KEY = 'AIzaSyA8qHwF1krHNFP0N8A4gZEnyLLSvO9iPcw'
 CURRENT_FEATURES = ['atm','bank','bus_station','church','gas_station','hospital','mosque','pharmacy','restaurant','school','train_station']
